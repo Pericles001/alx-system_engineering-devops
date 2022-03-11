88a4ef9fa237620dc3adb06c7fdaba1bfcefb1b3
Pericles001 <periclesadjovi@gmail.com>
2022-03-11 07:21:45 -0800

    Resolved puppet-lint  error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #configure ssh config
 
 file_line{'Turn off passwd auth':
-path  => '/etc/ssh/ssh_config',
-line  => 'PasswordAuthentification no'
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentification no'
 }
 
 file_line{'Declare identity file':
-path  => '/etc/ssh/ssh_config',
-line  => 'IdentitiFile ~/.ssh/school'
+path => '/etc/ssh/ssh_config',
+line => 'IdentitiFile ~/.ssh/school'
 
 }
