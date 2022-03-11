e268da37f9d14758786b646ec8f4a32c121ec2bf
Pericles001 <periclesadjovi@gmail.com>
2022-03-11 06:18:40 -0800

    Done with task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#configure ssh config
+
+file_line{'Turn off passwd auth':
+path  => '/etc/ssh/ssh_config',
+line  => 'PasswordAuthentification no'
+}
+
+file_line{'Declare identity file':
+path  => '/etc/ssh/ssh_config',
+line  => 'IdentitiFile ~/.ssh/school'
+
+}
