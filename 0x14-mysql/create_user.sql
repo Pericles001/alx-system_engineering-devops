1cbec6de95a30e3afc41652e93e978f7d24f784f
Pericles001 <periclesadjovi@gmail.com>
2022-04-05 16:47:44 +0000

    Refactor file

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,5 +1,4 @@
-#!/usr/bin/rnv bash
-#Create user holberton and give access to primary/replica status of the database
+-- Create user holberton and give access to primary/replica status of the database
 CREATE IF NO EXISTS `holberton_user`@`localhost` IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE,  ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
