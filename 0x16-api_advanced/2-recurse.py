e8858a7fe27585e965bfbd3fc31e1489547e5846
pericles001 <periclesadjovi@outlook.fr>
2022-04-27 03:59:14 +0100

    Formated file to be pycodestyle compliant

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,8 +2,8 @@
 """recursive function that queries the Reddit API and returns
  a list containing the titles of all hot articles for a given subreddit. """
 
-
 import requests
+
 
 def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
