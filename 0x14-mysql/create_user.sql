640c2d7bcb73ccf0c4e9a3c498fd42070a4b4b77
pericles001 <periclesadjovi@outlook.fr>
2022-05-04 17:50:33 +0100

    Reviewed script content

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -1,4 +1,9 @@
 -- Create user holberton and give access to primary/replica status of the database
-CREATE IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE,  ON *.* TO 'holberton_user'@'localhost';
+CREATE USER
+ IF NOT EXISTS 'holberton_user'@'localhost' 
+ IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE
+  ON * . * 
+  TO 'holberton_user'@'localhost'
+  IDENTIFIED BY 'projectcorrection280hbtn';
 FLUSH PRIVILEGES;
