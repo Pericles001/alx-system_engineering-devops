c578db2ce6bd20e1db575b8ca5f58a9ac1aac2eb
pericles001 <periclesadjovi@outlook.fr>
2022-05-04 18:02:01 +0100

    remove field for identification

--- 0x14-mysql/create_user.sql
+++ 0x14-mysql/create_user.sql
@@ -4,6 +4,5 @@ CREATE USER
  IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT SELECT,INSERT,UPDATE, REPLICATION CLIENT, REPLICATION SLAVE
   ON * . * 
-  TO 'holberton_user'@'localhost'
-  IDENTIFIED BY 'projectcorrection280hbtn';
+  TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
