a4d16af88a3f24f588cb3bc65a6f79c40c7c5589
Pericles001 <periclesadjovi@gmail.com>
2022-05-04 15:26:07 +0000

    Reviewed db creation script

--- 0x14-mysql/create_db.sql
+++ 0x14-mysql/create_db.sql
@@ -4,6 +4,6 @@ CREATE DATABASE
     IF NOT EXISTS `tyrell_corp`;
 CREATE TABLE IF NOT EXISTS `tyrell_corp`.`nexus6`(
     PRIMARY KEY(`id`),
-    `id` INT NOT NULL AUTOINCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
